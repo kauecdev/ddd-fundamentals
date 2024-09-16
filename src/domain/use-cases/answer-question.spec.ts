8f96255e7aeb1a15f82addc34a0eb0af3e7289b2
Kauê Cavalcante <kaue_cavalcante.cnt@outlook.com>
2024-09-16 10:21:06 -0300

    init: created base entities

---
+++ src/domain/use-cases/answer-question.spec.ts
@@ -0,0 +1,21 @@
+import { AnswerQuestionUseCase } from './answer-question'
+import { AnswersRepository } from '../repositories/answers-repository'
+import { Answer } from '../entities/answer'
+
+const fakeAnswersRepository: AnswersRepository = {
+  create: async (answer: Answer) => {return}
+}
+
+describe('Answer Question Use Case', () => {
+  it('should create an answer', async () => {
+    const answerQuestionUseCase = new AnswerQuestionUseCase(fakeAnswersRepository)
+
+    const answer = await answerQuestionUseCase.execute({
+      questionId: '1',
+      instructorId: '1',
+      content: 'Nova resposta'
+    })
+
+    expect(answer.content).toEqual('Nova resposta')
+  })
+})
