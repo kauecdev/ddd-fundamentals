86a30e3ff1d2f1dc3ec7db2bdc34aa5e65f4f9d0
Kauê Cavalcante <kaue_cavalcante.cnt@outlook.com>
2024-09-19 22:08:22 -0300

    feat: created factory function to make questions and added fake-js lib to create fake data for tests

---
+++ test/factories/make-question.ts
@@ -0,0 +1,18 @@
+import { faker } from '@faker-js/faker'
+
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+import { Question, QuestionProps } from "@/domain/forum/enterprise/entities/question";
+
+export function makeQuesion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityId,
+) {
+  const question = Question.create({
+    title: faker.lorem.sentence(),
+    authorId: new UniqueEntityId(),
+    content: faker.lorem.text(),
+    ...override
+  }, id)
+
+  return question
+}
