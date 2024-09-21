224ccd70a5f1b6396a747953aaf570a13bb213a1
Kauê Cavalcante <kaue_cavalcante.cnt@outlook.com>
2024-09-20 21:47:45 -0300

    feat: created entity for answer comment, question comment and comment as base class

---
+++ src/domain/forum/enterprise/entities/question-comment.ts
@@ -0,0 +1,29 @@
+import { Entity } from "@/core/entities/entity";
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+import { Optional } from "@/core/types/optional";
+import { Comment, CommentProps } from "./comment";
+
+interface QuestionCommentProps extends CommentProps {
+  questionId: UniqueEntityId
+}
+
+export class QuestionComment extends Comment<QuestionCommentProps> {
+  get questionId() {
+    return this.props.questionId
+  }
+
+  static create(
+    props: Optional<QuestionCommentProps, 'createdAt'>,
+    id?: UniqueEntityId
+  ) {
+    const questionComment = new QuestionComment(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id
+    )
+
+    return questionComment
+  }
+}
