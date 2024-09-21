98e2446f931fa78dbbc8d2a75c46a7eac13f1a30
Kauê Cavalcante <kaue_cavalcante.cnt@outlook.com>
2024-09-21 19:27:10 -0300

    feat: added either types to work with function error handling

---
+++ src/core/either.spec.ts
@@ -0,0 +1,27 @@
+import { Either, left, right } from './either'
+
+function doSomething(shouldSuccess: boolean): Either<string, number> {
+  if (shouldSuccess) {
+    return right(10)
+  }
+
+  return left('error')
+}
+
+describe('Either', () => {
+  it('should return success result', () => {
+    const result = doSomething(true)
+
+    expect(result.value).toEqual('success')
+    expect(result.isLeft()).toBe(false)
+    expect(result.isRight()).toBe(true)
+  })
+
+  it('should return error result', () => {
+    const result = doSomething(false)
+
+    expect(result.value).toEqual('error')
+    expect(result.isLeft()).toBe(true)
+    expect(result.isRight()).toBe(false)
+  })
+})
